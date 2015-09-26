c699547c69a1d9fd3cecb510e3bcd3857f5e566a
jackarmley <jack_armley@yahoo.co.uk>
2015-09-26 18:58:18 +0100

    design time

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -3,16 +3,19 @@
  * @author Nik, Jenny, Jack, Kabu
  */
 
-function storyform(){
-    self = this;
-    self.init();
-}
-
-storyform.prototype.init = function(){
-    console.log(self);
-};
-storyform.prototype.addStory = function(){
-
-};
-
-var mystory = new storyform();
+$('#filter-time').change(function(){
+    var that = $(this),
+        val = that.val(),
+        target = $('#filter-time-label');
+    target.text(val);
+});
+$('#ihave-skill').click(function(){
+    $('.filter-section--select-skill').addClass('state--active');
+    $(this).attr('selected','selected');
+    $('#ihave-goodwill').attr('selected',false);
+});
+$('#ihave-goodwill').click(function(){
+    $('.filter-section--select-skill').removeClass('state--active');
+    $(this).attr('selected','selected');
+    $('#ihave-skill').attr('selected',false);
+});
