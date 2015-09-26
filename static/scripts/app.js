6efe862777b1cae00c579fb551014e93f6df033a
jackarmley <jack_armley@yahoo.co.uk>
2015-09-26 14:33:43 +0100

    initial commit

---
+++ static/scripts/app.js
@@ -0,0 +1,18 @@
+/**
+ * Gender equality hack scripts
+ * @author Nik, Jenny, Jack, Kabu
+ */
+
+function storyform(){
+    self = this;
+    self.init();
+}
+
+storyform.prototype.init = function(){
+    console.log(self);
+};
+storyform.prototype.addStory = function(){
+
+};
+
+var mystory = new storyform();
