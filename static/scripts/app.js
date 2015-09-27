4b607104fe528b26999788534fdabc4388e2bbbb
jackarmley <jack_armley@yahoo.co.uk>
2015-09-27 10:35:07 +0100

    more styling

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -3,6 +3,8 @@
  * @author Nik, Jenny, Jack, Kabu
  */
 
+var challengesAccepted = 0;
+var countAppended = false;
 $('#filter-time').change(function(){
     var that = $(this),
         val = that.val(),
@@ -24,4 +26,21 @@ $('#ihave-goodwill').click(function(){
     $('.filter-section--select-skill').removeClass('state--active');
     $(this).attr('selected','selected');
     $('#ihave-skill').attr('selected',false);
+});
+
+$('.challengecard-cta').click(function(e){
+    var count = '<span class="challengecount" />'
+    var flag = '<span class="accepted icon-heart" />';
+    var accepted = $(this).parent().hasClass('state--accepted');
+    if(!accepted){
+        challengesAccepted++;
+        if(!countAppended){
+            $('.navitem-challenges').append(count);
+            countAppended = true;
+        }
+        $('.challengecount').text(challengesAccepted);
+        $(this).find('span').text('Challenge Accepted!');
+        $(this).parent().addClass('state--accepted').append(flag);
+    }
+    e.preventDefault();
 });
